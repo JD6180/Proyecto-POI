53a5aeeaa567d91be8038db75e9727875edcee37
JD6180 <julian.d.ortiz.a@gmail.com>
2021-10-23 20:14:09 -0500

    Back up

--- app/src/main/java/com/example/colombiadiversa/MapsFragment.kt
+++ app/src/main/java/com/example/colombiadiversa/MapsFragment.kt
@@ -1,21 +1,17 @@
 package com.example.colombiadiversa
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
-
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment(), GoogleMap.OnMarkerClickListener {
 
